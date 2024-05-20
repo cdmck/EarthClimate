7b2da0e76c3034c4dba59779b12b1a21078e9165
Conor McKenna <cmckenna003@gmail.com>
2024-05-20 00:20:15 -0400

    maybe progress

--- EarthCycle.py
+++ EarthCycle.py
@@ -1,2 +1,21 @@
-print ("hello world")
-print("hey")
+import pandas as pd
+import matplotlib.pyplot as plt
+
+df = pd.read_excel('C:\\Users\\cmcke\\OneDrive\\Desktop\\COS\\personal\\EarthClimateModel_VS\\earthmodel.xlsx', sheet_name='Model')
+
+data = {
+    'x': [1,2,3],
+    'y': [3,4,5],
+    'val': [10,20,30]
+}
+df = pd.DataFrame(data)
+
+plt.figure(figsize=(10,6))
+sc = plt.scatter(df['x'], df['y'], c=df['val'], cmap='viridis', s=100)
+
+plt.colorbar(sc, labels='val')
+
+plt.xlabel('Xax')
+plt.ylabel('Yax')
+
+plt.show()
